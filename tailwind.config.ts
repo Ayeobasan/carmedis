354153e28beffac10e82ac4605f866d7373fe14b
Johnnie.Js <97920146+Ayeobasan@users.noreply.github.com>
2024-08-18 20:39:48 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,12 +18,6 @@ const config = {
       },
     },
     extend: {
-      keyframes: {
-        fadeInUp: {
-          '0%': { opacity: '0', transform: 'translateY(20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-      },
       animation: {
         fadeInUp: 'fadeInUp 1s ease-out',
       },
