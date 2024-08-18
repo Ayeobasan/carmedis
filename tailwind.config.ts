0b4a3e86da821f8b132f17ef0cca39033745a872
Ayeobasan John <johnayeobasan@gmail.com>
2024-08-18 22:27:44 +0100

    only scanpro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,12 @@ const config = {
       },
     },
     extend: {
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
       animation: {
         fadeInUp: 'fadeInUp 1s ease-out',
       },
